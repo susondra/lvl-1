5993ac92c0f4e7e92b445b2172d26f3ad9936ab5
susondra <123081753+susondra@users.noreply.github.com>
2023-02-01 17:24:57 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,17 +2,30 @@ input.onButtonPressed(Button.A, function () {
     led.plot(x, y)
     x += 1
 })
-input.onGesture(Gesture.ScreenUp, function () {
-	
-})
 input.onButtonPressed(Button.B, function () {
-    x = 0
-    y += -1
+    x += 1
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    led.setBrightness(128)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    x = 0
+    y = 4
+    led.setBrightness(255)
 })
 let y = 0
 let x = 0
 x = 0
 y = 4
+basic.forever(function () {
+    if (x == 5) {
+        x = 0
+        y += -1
+    }
+})
